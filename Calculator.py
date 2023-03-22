5066acb6322d223ea4310603ae55454981c770e7
jesiresam <127721425+jesiresam@users.noreply.github.com>
2023-03-22 08:29:38 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+operator = (input("Enter operator:"))
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1-num2)
+elif operator == '*':
+    print(num1*num2)
+elif operator =='/':
+    print(num1/num2)
+else:
+    print("Invalid input, try again")
